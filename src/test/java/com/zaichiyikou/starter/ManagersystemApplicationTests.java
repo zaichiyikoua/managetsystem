2e81fbd9e6e8c5a5b4a349eeb78ac2ea591d1b68
zaichiyikou <913475430@qq.com>
2020-04-13 15:17:37 +0800

    2020-4-13 初始化项目

---
+++ src/test/java/com/zaichiyikou/starter/ManagersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaichiyikou.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
